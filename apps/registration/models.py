7faf0a47336c8fd7252bdec6c3950bce83cae2f2
willieshubert <willie.pictograms@gmail.com>
2017-02-22 13:41:52 -0800

    Login is functional

--- apps/registration/models.py
+++ apps/registration/models.py
@@ -7,48 +7,48 @@ Name_Regex = re.compile(r'^[A-Za-z]+$')
 
 # Create your models here.
 class userManager(models.Manager):
-    def validate (self, postFirstName, postLastName, postEmail, postPassword, postConfirm):
+    def validate (self, postData):
         errors = []
-        if not Email_Regex.match(postEmail):
+        if not Email_Regex.match(postData['email']):
             errors.append("Invalid email")
-        if len(User.objects.filter(email = postEmail)) > 0:
+        if len(User.objects.filter(email = postData['email'])) > 0:
             errors.append("Email already exists in our database")
-        if postPassword != postConfirm:
+        if postData['password'] != postData['confirm_password']:
             errors.append("Your passwords don't match")
-        if len(postFirstName) < 2:
+        if len(postData['first_name']) < 2:
             errors.append("Is that really your first name?")
-        if len(postLastName) < 2:
+        if len(postData['last_name']) < 2:
             errors.append("Is that really your last name?")
-        if not Name_Regex.match(postFirstName):
+        if not Name_Regex.match(postData['first_name']):
             errors.append("Is that number really in your first name?")
-        if not Name_Regex.match(postLastName):
-            errors.append("Is that number really in your first name?")
-        if len(postPassword) < 8:
+        if not Name_Regex.match(postData['last_name']):
+            errors.append("Is that number really in your last name?")
+        if len(postData['password']) < 8:
             errors.append("Password too short")
         if len(errors) == 0:
-            password = postPassword
-            hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
             #create the user
-            newuser = User.objects.create(first_name= postFirstName, last_name= postLastName, email= postEmail, password= hashed)
+            newuser = User.objects.create(first_name= postData['first_name'], last_name= postData['last_name'], email= postData['email'], password= bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt()))
             return (True, newuser)
         else:
             return (False, errors)
 
-
-    def login(self, postEmail, postPassword):
+    def login(self, postData):
         errors1 = []
-        user = User.objects.filter(email = postEmail)
-
-        if not Email_Regex.match(postEmail):
-            errors1.append("Invalid email")
-        elif len(postPassword) < 8:
-            errors1.append("Password too short")
+        if 'email' in postData and 'password' in postData:
+            try:
+                print 50*('8')
+                user = User.objects.get(email = postData['email'])
+            except User.DoesNotExist:
+                print 50*('4')
+                errors1.append("Sorry please try again")
+                return (False, errors1)
+        pw_match = bcrypt.hashpw(postData['password'].encode(), user.password.encode())
+        print 10*"3", user.password
+        if pw_match == user.password:
+            return (True, user)
+        else:
+            errors1.append("Sorry please try again!!!!")
             return (False, errors1)
-
-        if len(errors1) == 0: #if we have no errors..
-            if bcrypt.hashpw(postPassword.encode(), user[0].password.encode()) == user[0].password:
-                return (True, user )
-
 class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import User
 from django.contrib import messages
+
 # Create your views here.
 def index(request):
     return render(request, 'registration/index.html')
@@ -9,7 +10,7 @@ def success(request):
     if 'userid' not in request.session:
         return redirect ("/")
     context = {
-        'user' : User.objects.get(id= request.session['userid'])
+        'user' : User.objects.get(id=request.session['userid'])
     }
     print context['user'].first_name
     return render(request, 'registration/success.html', context)
@@ -17,7 +18,7 @@ def success(request):
 def register(request):
     if request.method == 'GET':
         return redirect ('/')
-    newuser = User.objects.validate(request.POST['first_name'], request.POST['last_name'],request.POST['email'],request.POST['password'],request.POST['confirm_password'])
+    newuser = User.objects.validate(request.POST)
     print newuser
     if newuser[0] == False:
         for each in newuser[1]:
@@ -29,30 +30,75 @@ def register(request):
         return redirect('/success')
 
 def login(request):
+    # if 'userid' in request.session:
+    #     return redirect('/success')
     if request.method == 'GET':
         return redirect('/')
-    user = User.objects.login(request.POST['email'], request.POST['password'])
-    print user
-    if user[0] == False:
-        for each in user[1]:
-            messages.errors1(request, each)
-        return redirect('/')
-    if user[0] == True:
-        messages.success(request, 'Welcome, You are logged in!')
-        request.session['userid'] = user[1].id
-        return redirect('/success')
+    else:
+        user = User.objects.login(request.POST)
+        print user
+        if user[0] == False:
+            for each in user[1]:
+                messages.add_message(request, messages.INFO, each)
+            return redirect('/')
+        if user[0] == True:
+            messages.add_message(request, messages.INFO,'Welcome, You are logged in!')
+            request.session['userid'] = user[1].id
+            return redirect('/success')
 
 
 def logout(request):
     if 'userid' not in request.session:
         return redirect('/')
-    # if not request.user.is_authenticated:
-    #     return redirect('/')
+    print "*******"
+    print request.session['userid']
     del request.session['userid']
     return redirect('/')
 
-def delete(request, id):
-    if 'userid' not in request.session:
-        return redirect('/')
-    User.objects.filter().delete()
-    return('/logout')
+#
+# def logout_view(request):
+#     logout(request)
+#     return redirect('/')
+#
+# def login_view(request):
+#     if request.method == 'GET':
+#         return redirect('/')
+#     if 'userid' in request.session:
+#         return redirect('/success')
+#     email = request.POST['email']
+#     password = request.POST['password']
+#     user = authenticate(email=email, password=password)
+#     if user is not None:
+#         login(request, user)
+#         messages.success(request, 'Welcome, You are logged in!')
+#         return redirect('/success')
+#     else:
+#         messages.errors(request, each)
+#         return redirect('/')
+#
+#
+# def login(request):
+#     if 'userid' in request.session:
+#         return redirect('/success')
+#     if request.method == 'GET':
+#         return redirect('/')
+#     user = User.objects.login(request.POST['email'], request.POST['password'])
+#     print user
+#     if user[0] == False:
+#         for each in user[1]:
+#             messages.errors1(request, each)
+#         return redirect('/')
+#     if user[0] == True:
+#         messages.success(request, 'Welcome, You are logged in!')
+#         request.session['userid'] = user[1].id
+#         return redirect('/success')
+
+
+
+
+
+# def delete(request, id):
+#     if 'userid' not in request.session:
+#         return redirect('/')
+#     User.objects.filter().delete()
+#     return('/logout')

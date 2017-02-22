9c5bc39fdef3cc05781c99a864bead847e81a201
willieshubert <willie.pictograms@gmail.com>
2017-02-21 16:06:51 -0800

    incomplete save

--- apps/registration/models.py
+++ apps/registration/models.py
@@ -1,5 +1,39 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import re
+import bcrypt
+Email_Regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+Name_Regex = re.compile(r'^[A-Za-z]+$')
 
 # Create your models here.
+class userManager(models.Manager):
+    def validate (self, postFirstName, postLastName, postEmail, postPassword, postConfirm):
+        if not Email_Regex.match(postEmail):
+            return {'errors':'False'}
+        elif len(User.userManager.filter(email = postEmail)) > 0:
+            return {'errors1': 'False'}
+        elif postPassword != postConfirm:
+            return {'errors2': 'False'}
+        elif len(postFirstName) < 2:
+            return {'errors3': 'False'}
+        elif len(postLastName) < 2:
+            return {'errors3': 'False'}
+        elif not Name_Regex.match(postFirstName):
+            return {'errors4': 'False'}
+        elif not Name_Regex.match(postLastName):
+            return {'errors4': 'False'}
+        elif len(postPassword) < 8:
+            return {'errors5': 'False'}
+        else:
+            password = postPassword
+            hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        return {'first_name': postFirstName, 'last_name': postLastName, 'email': postEmail, 'password': hashed}
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    userManager = UserManager()
--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'registration/index.html')
+
+def register(request):
+    if request.method == 'Get'
+        return redirect ('/')
+    user = User.userManager.validate(request.POST['first_name'], request.POST['last_name'],request.POST['email'],request.POST['password'],request.POST['confirm_password'],)
+
+
+    return redirect('/')
+
+def logout(request):
+    if 'userid' not in request.session:
+        return redirect('/')
+    del request.session['userid']
+    return redirect('/')
+
+def delete(request, id):
+    if 'userid' not in request.session:
+        return redirect('/')
+    User.userManager.filter(id=id).delete()
+    return redirect('/logout')
+#
